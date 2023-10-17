d7c88871410c93adebbf5b763d12ad5434d84e4f
LoweBerg <126024255+LoweBerg@users.noreply.github.com>
2023-10-17 18:41:04 +0200

    msg

--- threeFriends.cpp
+++ threeFriends.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     string input = "ABABABABA";
 
-    if(input.length() % 2 == 0) {
+    if(input.length() % 2 == 0 || input.length() < 3) {
         cout << "not possible";
         return 0;
     }
@@ -17,10 +17,13 @@ int main() {
     int p2 = input.length() / 2;
 
     // error in left
-    while(p2 < input.length()) {
+    while(p2 < input.length() && p1 < p2) {
         if(p1 != p2) {
             lwrongs++;
             p1++;
+        } else {
+            p1++;
+            p2++;
         }
     }
 
@@ -34,6 +37,9 @@ int main() {
     while(p2 < input.length()) {
         if(p1 != p2) {
             rwrongs++;
+            p2++;
+        } else {
+            p1++;
             p2++;
         }
     }
