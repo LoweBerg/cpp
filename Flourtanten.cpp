fc87bda5d7e15b1492b6d7c6869e2a13c8937cbb
Lowe Berg <loweberg04@gmail.com>
2023-10-18 10:21:07 +0200

    -

--- Flourtanten.cpp
+++ Flourtanten.cpp
@@ -19,29 +19,34 @@ int main() {
         else
             q[i] = input;
     }
-    /*
     int l = 0;
     int r = q.size();
     int m1;
     int m2;
+    int lsum;
+    int rsum;
 
     while(l < r) {
         m1 = l + (r-l)/3;
         m2 = r - (r-l)/3;
+        lsum = bjornSum(q, m1);
+        rsum = bjornSum(q, m1);
+        
 
-        if(bjornSum(q, m1) == bjornSum(q, m2)) {
+        if(lsum == rsum) {
             l = m1;
             r = m2;
-        } else if(bjornSum(q, m1) < bjornSum(q, m2)) {
+        } else if(lsum < rsum) {
             l = m1 + 1;
         } else {
             r = m2 - 1;
         }
     }
     cout << bjornSum(q, l);
-    */
 
+    /*
    cout << max(bjornSum(q, 0), bjornSum(q, q.size()));
+    */
 }
 
 int bjornSum(vector<int> q, int p) {
