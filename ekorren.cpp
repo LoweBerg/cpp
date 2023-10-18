9b84ce910cc52f7a4cfe15ac7d9e68a00759a689
Lowe Berg <loweberg04rgmail.com>
2023-10-18 21:54:35 +0200

    ugh

--- ekorren.cpp
+++ ekorren.cpp
@@ -5,30 +5,44 @@
 
 using namespace std;
 
-void bfs(vector<vector<int>>* matrix, int start) {
+int bfs(vector<vector<int>>* adj, int start, int dist, vector<int>* nuts) {
 
-    vector<bool> visited((*matrix).size(), false);
+    vector<bool> visited((*adj).size(), false);
     queue<tuple<int, int>> nodes;
-    nodes.push(tuple(0, 0));
+    nodes.push(tuple(start, dist));
+    visited[start] = true;
     int current;
-    int dist;
 
     while (nodes.size() > 0) {
 
+
         current = get<0>(nodes.front());
+        dist = get<1>(nodes.front()) + 1;
         nodes.pop();
 
-        for (int node : (*matrix)[current]) {
+        for(int node : (*adj)[current]) {
             if (visited[node]) {
-                continue;
+                goto cont;
             }
 
+            nodes.push(tuple(node, dist));
+            visited[node] = true;
 
+            if((*nuts).size() > 0) {
+                for(int i = 0; i < (*nuts).size(); i++) {
+                    if((*nuts)[i] == node) {
+                        (*nuts).erase((*nuts).begin() + i);
+                        return bfs(adj, node, dist, nuts);
+                    }
+                }
+            } else if(node == 0) {
+                return dist;
+            }
+            cont:;
         }
-
     }
-    
 
+    return -1;
 }
 
 int main() {
@@ -40,7 +54,7 @@ int main() {
     cin >> nodes;
     cin >> nuts_len;
 
-    vector<vector<int>> matrix(nodes, vector<int>());
+    vector<vector<int>> adj(nodes, vector<int>());
     vector<int> nuts;
     int nut;
     int node1;
@@ -48,7 +62,7 @@ int main() {
 
     for (int i = 0; i < nuts_len; i++) {
         cin >> nut;
-        nuts.push_back(nut);
+        nuts.push_back(nut - 1);
     }
 
     for (int i = 1; i < nodes; i++) {
@@ -57,7 +71,9 @@ int main() {
         cin >> node2;
         node2--;
 
-        matrix[node1].push_back(node2);
-        matrix[node2].push_back(node1);
+        adj[node1].push_back(node2);
+        adj[node2].push_back(node1);
     }
+
+    cout << bfs(&adj, 0, 0, &nuts);
 }
