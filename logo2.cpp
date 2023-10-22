546e87e86f6ae9b7f755bddc9a90748df6402c8e
Lowe Berg <loweberg04rgmail.com>
2023-10-23 00:08:07 +0200

    i hate logo2

--- logo2.cpp
+++ logo2.cpp
@@ -4,79 +4,86 @@
 
 using namespace std;
 
-int main() {
-    double toRadians = acos(-1)/180;
-    double toDegrees = 180/acos(-1);
-    
-    vector<double> t(2);
-    int v;
+int findDist(vector<vector<string>> moves) {
+    double a = 0;
+    double x = 0;
+    double y = 0;
 
+    for(vector<string> m : moves) {
+        if(m[1] == "?")
+            continue;
+        if(m[0] == "fd") { 
+            x += stod(m[1]) * cos(a);
+            y += stod(m[1]) * sin(a);
+        } else if(m[0] == "bk") {
+            x -= stod(m[1]) * cos(a);
+            y -= stod(m[1]) * sin(a);
+        } else if(m[0] == "lt") {
+            a += stod(m[1]) * acos(-1)/180; 
+        } else {
+            a -= stod(m[1]) * acos(-1)/180; 
+        }
+    }
+
+    return round(sqrt(pow(0-x,2) + pow(0-y,2)));
+}
+
+int findAngle(vector<vector<string>> moves) {
+    double min_dist = 1e-9;
+
+    for(int i = 100; i < 360; i++) {
+        double a = 0;
+        double x = 0;
+        double y = 0;
+        for(vector<string> m : moves) {
+            string m0 = m[0];
+            string m1 = m[1];
+            m1 = (m1 == "?") ? to_string(i) : m1;
+
+            if(m0 == "fd") {
+                x += stod(m1) * cos(a);
+                y += stod(m1) * sin(a);
+            } else if(m0 == "bk") {
+                x -= stod(m1) * cos(a);
+                y -= stod(m1) * cos(a);
+            } else if(m0 == "lt") {
+                a += stod(m1) * acos(-1)/180;
+            } else {
+                a -= stod(m1) * acos(-1)/180;
+            }
+        }
+
+        if(sqrt(pow(0-x,2) + pow(0-y,2)) < min_dist)
+            return i;
+    }
+    return 0;
+}
+
+int main() {
+    
     int cases;
     cin >> cases;
-
-    string halt_move;
-    vector<double> halt_pos;    
     
-    string input;
-    vector<vector<string>> moves;
-
-    vector<double> output;
+    bool findA = false;
     
     vector<string> m;
     int movesLen;
     for(int _ = 0; _ < cases; _++) {
-        v = 0;
-        t[0] = 0;
-        t[1] = 0;
         cin >> movesLen;
-        
+        vector<vector<string>> moves(movesLen);
         for(int i = 0; i < movesLen; i++) {
-            m.clear();
-            cin >> input;
-            m.push_back(input);
-            cin >> input;
-            m.push_back(input);
-            if(m[1] == "?") {
-                halt_pos = t; 
-                halt_move = m[0];
-                goto cont;
-            }
-            if(m[0] == "fd") {
-                t[0] += stod(m[1]) * cos(v*toRadians);
-                t[1] += stod(m[1]) * sin(v*toRadians);
-            }
-            else if(m[0] == "bk") {
-                t[0] -= stod(m[1]) * cos(v*toRadians);
-                t[1] -= stod(m[1]) * sin(v*toRadians);
-            } else if(m[0] == "lt") {
-                v += stod(m[1]);
-            } else {
-                v -= stod(m[1]);
-            }
-            
-            cont:;
+            string m;
+            cin >> m;
+            moves[i].push_back(m);
+            cin >> m;
+            moves[i].push_back(m);
+            if(moves[i][1] == "?")
+                findA = (moves[i][0] == "lt" || moves[i][0] == "rt");
         }
-
-        if(halt_move == "fd" || halt_move == "bk") {
-            output.push_back(sqrt(pow(0 - t[0], 2) + pow(0 - t[1], 2)));
-        } else {
-            double a = ((t[0]-halt_pos[0])*(0-halt_pos[0]) + (t[1]-halt_pos[1])*(0-halt_pos[1]));
-            double b = (sqrt(pow(t[0] - halt_pos[0], 2) + pow(t[1] - halt_pos[1], 2)) * sqrt(pow(0 - halt_pos[0], 2) + pow(0 - halt_pos[1], 2)));
-            if(t[0] * t[1] < 0) {
-                if(halt_move == "rt")
-                    output.push_back(acos(a/b)*toDegrees);
-                else
-                    output.push_back(360 - acos(a/b)*toDegrees);                   
-            } else {
-                if(halt_move == "rt")
-                    output.push_back(360 - acos(a/b)*toDegrees);
-                else
-                    output.push_back(acos(a/b)*toDegrees);
-            }
-        }
-    }
-
-    for(double n : output) {
-        cout << round(n) << endl;
+        if(findA)
+            cout << findAngle(moves);
+        else
+            cout << findDist(moves);
+        cout << endl;
     }
 }
