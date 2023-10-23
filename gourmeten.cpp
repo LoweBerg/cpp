f6d6c3fc3e53ea76c5874cfc5b038652d03558ea
Lowe Berg <loweberg04rgmail.com>
2023-10-23 21:41:57 +0200

    Merge branch 'main' of https://github.com/LoweBerg/cpp

--- gourmeten.cpp
+++ gourmeten.cpp
@@ -12,18 +12,19 @@ int main() {
 
     vector<int> t(n);
 
-    for(int e : t) {
+    for(int &e : t) {
         cin >> e;
     }
-    vector<int> dp(m);
+    vector<int> dp(m+1);
 
     for(int i = 0; i <= m; i++) {
         for(int j = 0; j < t.size(); j++) {
             if(i - t[j] == 0) {
                 dp[i]++;
             } else if(i - t[j] > 0) {
-                if(dp[i-t[j]] > 0)
+                if(dp[i-t[j]] > 0) {
                     dp[i] += dp[i-t[j]];
+                }
             }
         }
     }
