6e711ce0a587d7a9bc2d1789b5dfe8a6e31973f4
Lowe Berg <loweberg04rgmail.com>
2023-10-31 16:13:59 +0100

    f

--- problemA.cpp
+++ problemA.cpp
@@ -6,50 +6,6 @@
 
 using namespace std;
 
-
-int findLand(vector<vector<node*>> map, int start_x, int start_y, node* origin) {
-    int land = 0;
-
-    queue<vector<int>> q;
-    q.push({start_x, start_y});
-
-    bool valid = false;
-
-    while(!q.empty()) {
-        int c_x = q.front()[0], c_y = q.front()[1];
-        q.pop();
-        if((map[c_x][c_y])->getParent() == map[start_x][start_y]->getParent())
-            continue;
-        if(map[c_x][c_y]->getParent() == origin) {
-            valid = true;
-            continue;
-        }
-        map[c_x][c_y]->getParent()->setParent(map[start_x][start_y]->getParent());
-        map[c_x][c_y]->getParent()->children += map[c_x][c_y]->children + 1;
-        land++;
-
-        // check up
-        if(c_x-1 >= 0) {
-            q.push({c_x-1,c_y});
-        }
-        // check down
-        if(c_x+1 < map.size()) {
-            q.push({c_x+1,c_y});
-        }
-        // check left
-        if(c_y-1 >= 0) {
-            q.push({c_x,c_y-1});
-        }
-        // check right
-        if(c_y+1 < map[c_x].size()) {
-            q.push({c_x,c_y+1});
-        }
-        
-    }
-
-    return (valid) ? land : -1;
-}
-
 struct node {
     int children = 0;
     char val;
@@ -70,6 +26,72 @@ struct node {
     }
 };
 
+int findLand(vector<vector<node*>> map, int start_x, int start_y, node* origin) {
+
+    queue<vector<int>> q;
+
+    // check up
+    if(start_x-1 >= 0 && map[start_x-1][start_y]->val == '#') {
+        q.push({start_x-1,start_y});
+    }
+    // check down
+    if(start_x+1 < map.size() && map[start_x+1][start_y]->val == '#') {
+        q.push({start_x+1,start_y});
+    }
+    // check left
+    if(start_y-1 >= 0 && map[start_x][start_y-1]->val == '#') {
+        q.push({start_x,start_y-1});
+    }
+    // check right
+    if(start_y+1 < map[start_x].size() && map[start_x][start_y+1]->val == '#') {
+        q.push({start_x,start_y+1});
+    }
+
+    bool valid = false;
+
+    while(!q.empty()) {
+        int c_x = q.front()[0], c_y = q.front()[1];
+        q.pop();
+        if(map[c_x][c_y]->val == '.')
+            continue;
+        if(map[c_x][c_y]->getParent() == map[start_x][start_y]->getParent())
+            continue;
+        if(map[c_x][c_y]->getParent() == origin) {
+            valid = true;
+            continue;
+        } else {
+            map[c_x][c_y]->getParent()->setParent(map[start_x][start_y]->getParent());
+            map[c_x][c_y]->getParent()->children += map[c_x][c_y]->children + 1;
+        }
+
+        // check up
+        if(c_x-1 >= 0 && map[c_x-1][c_y]->val == '#') {
+            q.push({c_x-1,c_y});
+        }
+        // check down
+        if(c_x+1 < map.size() && map[c_x+1][c_y]->val == '#') {
+            q.push({c_x+1,c_y});
+        }
+        // check left
+        if(c_y-1 >= 0 && map[c_x][c_y-1]->val == '#') {
+            q.push({c_x,c_y-1});
+        }
+        // check right
+        if(c_y+1 < map[c_x].size() && map[c_x][c_y+1]->val == '#') {
+            q.push({c_x,c_y+1});
+        }
+        
+    }
+
+    if(valid) {
+        map[start_x][start_y]->setParent(origin);
+        origin->children += map[start_x][start_y]->children + 1;
+    }
+
+    return origin->children+1;
+}
+
+
 int main() {
     int r, c, u;
     cin >> r;
@@ -86,14 +108,14 @@ int main() {
     for(int i = 0; i < r; i++) {
         cin >> input;
         for(int j = 0; j < c; j++) {
-            node n = node(input[j]);
-            n.setParent(&n);
+            node* n = new node(input[j]);
+            n->setParent(n);
             if(input[j] == 'S') {
                 s_x = i;
                 s_y = j;
-                origin = &n;
+                origin = n;
             }
-            map[i].push_back(&n);
+            map[i].push_back(n);
         }
     }
 
